8aa8177508190907fae0fab31c1b0e84694c6a46
Brian Chen <bcchen52@gmail.com>
2022-12-07 09:18:28 -0500

    s

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,18 @@ def index():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    ret
+    if request.method == "GET":
+        return render_template('login.html')
+    if request.method == "POST":
+        return render_template('home.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "GET":
+        return render_template('register.html')
+    if request.method == "POST":
+        return render_template('login.html')
+
 
 
 if __name__ == "__main__": #false if this file imported as module
