a5cb2a9691a014914d75147e60c043c6bcb7a960
Jeff <cytastorm@gmail.com>
2022-12-07 21:07:30 -0500

    weather api works

--- api_test.py
+++ api_test.py
@@ -1,6 +1,5 @@
 from flask import session, Flask, render_template, json
 from urllib.request import urlopen, Request
-from urllib import request
 
 app = Flask(__name__)
 
@@ -8,13 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
-    #request_site = Request(getMEME)#bundles url with headers to identify user as not a bot
+    request_site = Request("https://api.weatherbit.io/v2.0/current?city=New%20York%20City&key=dadc4ef5d8f84a6687a572237e934b84&units=I")#bundles url with headers to identify user as not a bot
     #print(URL)#checks for getting correct URL
     response = urlopen(request_site)#grabs the JSON from the page
     data_json = json.loads(response.read())#reads the JSON of the page and turns it into a dictionary
     print(data_json)#checks for correct retrieval of JSON
-
-    return render_template('main.html', image=data_json['body']['image'])
+    return render_template('api_test.html', temperature=data_json['data'][0]['temp'], humidity=data_json['data'][0]['rh'], rainChance=data_json['data'][0]['precip'])
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
