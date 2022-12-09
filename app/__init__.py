a9dc9601f92fffe79c1e502f037ed72123b15c04
vgraeber30 <vgraeber30@stuy.edu>
2022-12-09 09:18:35 -0500

    updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,33 +8,49 @@ app.secret_key = os.urandom(12)
 
 @app.route("/")
 def index():
-    session['logged_in'] = True
     return render_template('home.html')
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == "GET":
         return render_template('login.html')
+    usr = request.form["user"]
     if request.method == "POST":
 
         session.permanent = True
-        session["username"] = user
+        session["username"] = usr
         session['logged_in'] = True
 
-        return render_template('home.html')
+        return redirect(url_for("home"))
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == "GET":
         return render_template('register.html')
     if request.method == "POST":
-        return render_template('login.html')
+        return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
     session.pop('username', None)
+    return redirect(url_for("login"))
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/preferences")
+def preferences():
+    return render_template("preferences.html")
+
+@app.route("/grass")
+def grass():
+    return render_template("grass.html")
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
