e2f8607cb74a0d5fa58fb5ea6a357fd5719b25df
Jeff <cytastorm@gmail.com>
2022-12-12 20:18:05 -0500

    algorithm wip

---
+++ app/algorithm.py
@@ -0,0 +1,12 @@
+from database import get_anime_pref, get_league_pref, get_weather_pref
+from api_info import *
+def calc_weather(city):
+    weather = get_weather(city)
+    temp = weather['temperature']
+    humidity = weather['humidity']
+    rain_chance = weather['rain_chance']
+    
+def algorithm(preferences):
+    
+
+algorithm()
