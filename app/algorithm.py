fd4b2464c2db1aa5c94eb46c2d340c9af155d26f
Jeff <cytastorm@gmail.com>
2022-12-14 08:59:49 -0500

    commented

--- app/algorithm.py
+++ app/algorithm.py
@@ -56,17 +56,17 @@ def calc_anime_date(anime_id):
     
     
 def algorithm(uid):
-    print(calc_weather("New York City")) 
+    #print(calc_weather("New York City")) 
     #print(calc_weather("New York City") * get_weather_pref(uid) / 10) 
 
-    print(calc_LOL_clash())
+    #print(calc_LOL_clash())
     #print(calc_LOL_clash() * get_league_pref(uid) / 10)
     
-    print(calc_anime_date(44511)) #test using chainsawman
+    #print(calc_anime_date(44511)) #test using chainsawman
     #print(calc_anime_date(44511) * get_anime_pref(uid) / 10) #test using chainsawman
 
     return((calc_weather("New York City") * get_weather_pref(uid) / 10) +
             (calc_LOL_clash() * get_league_pref(uid) / 10) +
             (calc_anime_date(44511) * get_anime_pref(uid) / 10))
 
-algorithm(0)
+#algorithm(0)
